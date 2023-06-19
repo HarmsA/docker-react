461da34e42cfeccedf0097a8c87ff53e7623e269
Adam Harms <adamharms@MacBook-Pro.local>
2023-06-19 10:26:11 -0500

    app.js update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there changed again with help
+          Hi there changed again with help!
         </p>
         <a
           className="App-link"
